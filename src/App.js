bd915e7b2b7aeda742f309121a5ce1d3950dd46b
NEEMIAS BRAGA KELLY DOS SANTSO <neemiassantos217@gmail.com>
2024-02-28 14:25:10 -0300

    Desafio comcluido com sucesso!

--- src/App.js
+++ src/App.js
@@ -8,49 +8,117 @@ import { useState } from "react";
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('0');
-  const [sinal, setSinal] = useState('');
-  const [firstNumber, setFirstNumber] = useState('0');
+  const [currentNumber, setCurrentNumber] = useState('');
+  const[ operation, setOperation]=useState('')
+  const [inputmostre, setInputMostre] = useState('');
+  const [total, settotal] = useState('');
+  const [firstNumber, setFirstNumber] = useState('');
 
   const handleOnClear = () =>{
-      setCurrentNumber('0')
-      setFirstNumber('0')
+      setCurrentNumber('');
+      setFirstNumber('');
+      setOperation('');
+      setInputMostre('');
+      settotal('0');
   }
   const handleAddNumber = (num) =>{
-      setCurrentNumber(prev =>`${prev ==='0' ? '':prev}${num}`)
+      setCurrentNumber(prev =>`${prev ==='' ? '':prev}${num}`)
   }   
 
   const handleSumNumbers = (num) =>{
-    if (firstNumber === '0'){
+    if (firstNumber === ''){
       setFirstNumber(String(currentNumber));
-      setCurrentNumber(currentNumber)
-      setSinal("+")
-      
+      setInputMostre(String(currentNumber)+'+')
+      setCurrentNumber('');  
+      setOperation('+');
     }else{
       const sum = Number(firstNumber)+ Number(currentNumber);
-      setCurrentNumber(String(sum));
-      setFirstNumber('0')
-      setSinal("
-      ")
+      setCurrentNumber(String(sum)); 
+      setFirstNumber('');
     }
   }
+
+  const handleMinusNumbers = (num) =>{
+    if (firstNumber === ''){
+      setFirstNumber(String(currentNumber));
+      setInputMostre(String(currentNumber)+'-') 
+      setCurrentNumber(''); 
+      setOperation('-');
+    }else{
+      const sum = Number(firstNumber)- Number(currentNumber);
+      setCurrentNumber(String(sum)); 
+      setFirstNumber('');
+    }
+  }
+
+  const handleDivNumbers = (num) =>{
+    if (firstNumber === ''){
+      setFirstNumber(String(currentNumber));
+      setInputMostre(String(currentNumber)+'/')
+      setCurrentNumber('');  
+      setOperation('/');
+    }else{
+      const sum = Number(firstNumber)/ Number(currentNumber);
+      setCurrentNumber(String(sum)); 
+      setFirstNumber('');
+    }
+  }
+
+  const handleMultNumbers = (num) =>{
+    if (firstNumber === ''){
+      setFirstNumber(Number(currentNumber));
+      setInputMostre(String(currentNumber)+'x') 
+      setCurrentNumber(''); 
+      setOperation('x');
+    }else{
+      const sum = Number(firstNumber)* Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setFirstNumber('');
+    }
+  }
+
+
+
+   const handleEquals = () =>{
+
+    if(firstNumber !== '' && operation != '' && currentNumber != '' ){
+      switch (operation){
+        case '+':
+          handleSumNumbers();
+          setInputMostre('')
+          break;
+        case '-':
+            handleMinusNumbers();
+            setInputMostre('')
+            break;
+        case 'x':
+            handleMultNumbers();
+            setInputMostre('')
+            break;
+        case '/':
+           handleDivNumbers();
+           setInputMostre('')
+            break;   
+        default:
+          break; 
+      }
+    }
+
+
+  }
+
   return (
     <Container>
       <Content>
-      <Input value={currentNumber + sinal}/>
+      <Input value={inputmostre + currentNumber }/>
  
-      <Row>
-      <Button label="x" onClick={() => handleAddNumber('')}/>
-      <Button label="/" onClick={() => handleAddNumber('')}/>
-      <Button label="C" onClick={handleOnClear}/>
-      <Button label="X" onClick={() => handleAddNumber('')}/>
-      </Row>
+   
 
       <Row>
       <Button label="7" onClick={() => handleAddNumber('7')}/>
       <Button label="8" onClick={() => handleAddNumber('8')}/>
       <Button label="9" onClick={() => handleAddNumber('9')}/>
-      <Button label="-" onClick={() => handleAddNumber('')}/>
+      <Button label="-" onClick={handleMinusNumbers}/>
       </Row>
 
       <Row>
@@ -64,9 +132,18 @@ const App = () => {
       <Button label="1" onClick={() => handleAddNumber('1')}/>
       <Button label="2" onClick={() => handleAddNumber('2')}/>
       <Button label="3" onClick={() => handleAddNumber('3')}/>
-      <Button label="=" onClick={() => handleAddNumber('=')}/>
+      <Button label="0" onClick={() => handleAddNumber('0')}/>
       </Row>
 
+      <Row>
+      <Button label="x" onClick={ handleMultNumbers}/>
+      <Button label="/" onClick={handleDivNumbers}/>
+      <Button label="c" onClick={handleOnClear}/>
+      <Button label="." onClick={() => handleAddNumber('.')}/>
+      </Row>
+      <Row>
+      <Button label="=" style="background-color:#74ad5e; " onClick={handleEquals}/>
+      </Row>
       </Content>
       
     </Container>
